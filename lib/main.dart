86a5511a496956029b7f38a8ffd58b68b0f99871
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-15 22:45:59 +0530

    basic flutter added

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.deepPurple),
+        // Use Material 3 design language
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(45),
+                ),
+              ),
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+            ),
+            onPressed: () {
+              print('Text Button tapped!');
+            },
+            child: Text('Click Me'),
+          ),
+          Image.asset('assest/images/download.jpg'),
+          Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Color.fromARGB(255, 202, 245, 10),
+              child: Center(
+                child: Text(
+                  "WBJEE",
+                  style: TextStyle(color: Colors.pinkAccent),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
